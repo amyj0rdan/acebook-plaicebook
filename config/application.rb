cdeeeb8d65bfb1b74671b3bea6dc72c887ca27f4
henrygambles <gambles.henry2@gmail.com>
2019-05-03 14:30:06 +0100

    Fixed 'translation missing' sign up bug.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Acebook
     config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_view.raise_on_missing_translations = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
